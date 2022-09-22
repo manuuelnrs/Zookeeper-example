d6f4d9662f0f991031769e21a104e234523b03b9
AlfaroE <esau.alfaro00@gmail.com>
2022-09-21 20:00:11 -0500

    producto terminado

--- run.py
+++ run.py
@@ -1,9 +1,86 @@
+from ast import Delete
 from treelib import Node,Tree
+from zookeeper import Ztree
+from zookeeper import Znode 
 
 
-tree = Tree()
-tree.create_node("hola1",1)
-tree.create_node("hola2",2, parent= 1)
-tree.create_node("hola3",3, parent= 1)
-tree.create_node("hola4",4, parent= 2)
-tree.show()
+
+arbol = Ztree()
+
+while(True):
+    print("\t\t-------Bienvenido a Zookeeper--------")
+    print("*** Selecciona una opción *****")
+    print("1. Crear Znode")
+    print("2. Borrar Znode")
+    print("3. Ver arbol")
+    print("4. ver informacion de Znode")
+    print("5. Cambiar Data de Znode")
+    print("6. Mostrar hijos de Znode")
+    print("7. Buscar Znodo")
+    print("8. Ver data de Znode")
+    print("0. Salir")
+
+    opcion = input("$")
+
+    if(opcion == "0"):
+        break
+    if(opcion == "1"):
+        path = input("Path = ")
+        data = input("Contenido de Znode = ")
+        efimero = input("Es efimero? (True/False) =")
+        onservice = input("El servidor esta activo? (True/False) =")
+        deadtime = input("Deadtime =")
+        padre = input("Ruta del padre =")
+        if(efimero == "True"):
+            efimero = True
+        if(efimero == "False"):
+            efimero = False
+        if(onservice == "True"):
+            onservice = True
+        if(onservice == "False"):
+            onservice = False
+
+        arbol.create(path,data,efimero,onservice,int(deadtime),padre)
+    if(opcion == "2"):
+        path = input("Path = ")
+        version = input("Version actual =")
+        arbol.delete(path,int(version))
+    if(opcion == "3"):
+        arbol.showTree()
+    if(opcion =="4"):
+        path = input("Path = ")
+        arbol.showNode(path)
+    if(opcion == "5"):
+        path = input("Path = ")
+        contenido = input("Contenino =")
+        arbol.setData(path,contenido)
+    if (opcion=="6"):
+        path = input("Path = ")
+        print(arbol.getChildren(path))
+    if (opcion=="7"):
+        path = input("Path = ")
+        if(arbol.exist(path)):
+            print("Nodo encontrado")
+        else:
+            print("Nodo NO encontrado")
+    if(opcion == "8"):
+        path = input("Path = ")
+        print(arbol.getData(path))
+        
+
+
+
+
+#arbol.create("/Amazon",{"hola",123,"soy data"},False,True,None,"/")
+#arbol.create("/Nodo2",{"holiiiis"},True,True,120,"/")
+#arbol.create("/Amazon/Nodo3",{},False,True,None,"/Amazon")
+#arbol.create("/Amazon/Nodo4",{"contenido nuevo",2,"hey"},False,True,None,"/Amazon")
+#arbol.create("/Amazon/Nodo5",{"contenido nuevo",2,"hey"},False,True,None,"/Amazon/Nodo3")
+#arbol.showTree()
+
+#arbol.delete("/Nodo2",0)
+#print(arbol.getData("/Amazon"))
+#arbol.setData("/Amazon",{"contenido nuevo",2,"hey"})
+#arbol.showNode("/Amazon")
+#arbol.getChildren("/Amazon")
+
--- zookeeper.py
+++ zookeeper.py
@@ -9,31 +9,104 @@ class Ztree:
     def get_tree(self):
         return self.tree
 
-    def create(self,path,data,ephemeral,OnService):
-        return Znode(path,data,ephemeral,OnService)
+    def create(self,path,data,ephemeral,OnService,timeDead,parent):
+        
+        if(ephemeral):
+            self.tree.add_node(Node(path,path,data= Znode(path,data,ephemeral,OnService,timeDead)),parent= parent) 
+        else:
+            self.tree.add_node(Node(path,path,data= Znode(path,data,ephemeral,OnService,None)),parent= parent)
 
     def delete(self,path,version):
-        pass
+        if(self.exist(path)):
+            nodo = self.tree.get_node(path)
+            nodoData = nodo.data
+            if(nodoData.ephemeral == False):
+                if(self.tree.contains(path)):
+                    if(nodoData.version == version):
+                        self.tree.remove_node(path)
+                    else:
+                        print("Las versiones no concuerdan c:")
+            else:
+                if((nodoData.OnService == False) and (nodoData.ephemeral == True)):
+                    self.tree.remove_node(path)
+                    return 0
+                else:
+                    if((nodoData.OnService == True) and (nodoData.ephemeral == True)):
+                        for i in range(nodoData.timeDead,-1,-1):
+                            if(i == 0):
+                                if(nodoData.version == version):
+                                    self.tree.remove_node(path)
+                                else:
+                                    print("Las versiones no concuerdan c:")
+                            else:
+                                print("Tiempo restante de vida = {i}".format(i=i))
+        else:
+            print("Nodo no existe")
+            
+    def exist(self,path):
+        if(self.tree.contains(path)):
+            return True
+        else: 
+            return False
+    
+    def showTree(self):
+        print("\t\t -------Estado actual del Arbol--------")
+        self.tree.show()
+    
+    def getData(self,path):
+        if(self.exist(path)):
+            nodo = self.tree.get_node(path)
+            nodoData = nodo.data
+            return(nodoData.data)
+        else:
+            print("Nodo no existe")
+    
+    def setData(self,path,contenido):
+        if(self.exist(path)):
+            nodotemp = self.tree.get_node(path)
+            nodo = nodotemp.data
+            nodo.data = contenido
+            nodo.version = nodo.version + 1
+        else:
+            print("Nodo no existe")
+    
+    def showNode(self,path):
+        if(self.exist(path)):
+            nodotemp = self.tree.get_node(path)
+            nodo = nodotemp.data
+            nodo.get_node_info()
+        else:
+            print("Nodo no existe")
+    
+    def getChildren(self,path):
+        if(self.exist(path)):
+            nodotemp = self.tree.get_node(path)
+            print(self.tree.children(path))
+        else:
+            print("Nodo no existe")
+    
+
+    
 
 
 
 
 class Znode:
-    def __init__(self,path,data,ephemeral,OnService):
+    def __init__(self,path,data,ephemeral,OnService,timeDead):
         self.path = path
         self.ephemeral = ephemeral
         self.data = data
         self.node = Node(path,path)
         self.OnService = OnService 
+        self.timeDead = timeDead
         self.version = 0
-        self.timeDead = 60
     
     def get_node_info(self):
         print("\t \t------ZNode information--------\t\t \n")
-        print("Path = {path} \n".format(path = self.path))
-        print("Ephemeral = {ephemeral} \n".format(ephemeral = self.ephemeral))
-        print("Version = {version} \n".format(version = self.version))
-        print("Data = {data} \n".format(data = self.data))
+        print("Path = {path} ".format(path = self.path))
+        print("Ephemeral = {ephemeral} ".format(ephemeral = self.ephemeral))
+        print("Version = {version} ".format(version = self.version))
+        print("Data = {data} ".format(data = self.data))
 
         
         
